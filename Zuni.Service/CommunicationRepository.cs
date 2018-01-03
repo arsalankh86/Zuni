3c204f3a20b2bd7c5dda78835dadadfb93040d87
arsalankh86 <softwares.coders@gmail.com>
2018-01-04 03:05:16 +0500

    Add Communication Repository in

---
+++ Zuni.Service/CommunicationRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zuni.Service
+{
+    public class CommunicationRepository
+    {
+        public void SendEmail()
+        {
+
+        }
+
+        public void SendMessage()
+        {
+
+        }
+    }
+}
